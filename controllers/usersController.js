4c5bc3b990720a911fe0a8b4b80a66435ddc386b
msolorio <msolorio.github@gmail.com>
2021-01-20 14:30:33 -0800

    Add user sessions and bcrypt to hash user passwords

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const db = require('../models');
 
+// CURRENT ROUTE - /users
 
 ///////////////////////////////////////////////////////
 // SHOWS THE SIGNUP FORM
@@ -13,7 +15,25 @@ router.get('/signup', (req, res) => {
 ///////////////////////////////////////////////////////
 // HANDLES SUBMIT OF SIGNUP FORM
 router.post('/', (req, res) => {
-  res.redirect('/users/login');
+  console.log(req.body);
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return console.log(err);
+
+    bcrypt.hash(req.body.password, salt, (err, hashedPassword) => {
+      const newUser = {
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword
+      }
+    
+      db.User.create(newUser, (err, createdUser) => {
+        if (err) return console.log(err);
+    
+        res.redirect('/users/login');
+      })
+    })
+  })
 });
 
 
@@ -27,7 +47,36 @@ router.get('/login', (req, res) => {
 ///////////////////////////////////////////////////////
 // HANDLES SUBMIT OF LOGIN FORM
 router.post('/login', (req, res) => {
-  res.redirect('/users/login');
+  db.User.findOne({ username: req.body.username }, (err, foundUser) => {
+    console.log(req.body);
+    console.log(foundUser);
+
+    bcrypt.compare(req.body.password, foundUser.password, (err, result) => {
+      if (err) return console.log(err);
+
+      if (result) {
+        // console.log('Password matched!');
+
+        req.session.currentUser = foundUser;
+
+        res.redirect('/users/account-page');
+      } else {
+        res.redirect('/users/login');
+      }
+    })
+
+    // if (req.body.password === foundUser.password) {
+    //   // console.log(req.session);
+
+    //   req.session.currentUser = foundUser;
+
+    //   // console.log(req.session);
+
+    //   res.redirect('/users/account-page');
+    // } else {
+    //   res.redirect('/users/login');
+    // }
+  })
 });
 
 
@@ -40,9 +89,16 @@ router.get('/logout', (req, res) => {
 
 ///////////////////////////////////////////////////////
 // SHOWS USER'S ACCOUNT PAGE
-router.get('/:id', (req, res) => {
-  db.User.findById(req.params.id, (err, foundUser) => {
+router.get('/account-page', (req, res) => {
+
+  if (!req.session.currentUser) {
+    return res.redirect('/users/login');
+  }
+
+  db.User.findById(req.session.currentUser._id, (err, foundUser) => {
     if (err) return console.log(err);
+
+    // console.log(req.session);
 
     const context = {
       currentUser: foundUser
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const app = express();
 const PORT = 4000;
 const usersController = require('./controllers/usersController');
@@ -9,7 +10,14 @@ const usersController = require('./controllers/usersController');
 // Configuration / Middleware
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(session({
+  secret: 'milo the barking dog',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 * 2 // valid for 2 weeks
+  }
+}));
 
 /////////////////////////////////////////////////////////
 // Controller
